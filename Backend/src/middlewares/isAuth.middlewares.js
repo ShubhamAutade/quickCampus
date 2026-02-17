f6a5e31a0d02fe2812b48658f4a55fab6b622cdb
Shubham Autade <autades654@gmail.com>
2026-02-17 17:37:51 +0530

    feat: implement JWT verification middleware and populate req.user

---
+++ Backend/src/middlewares/isAuth.middlewares.js
@@ -0,0 +1,47 @@
+
+import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+
+export const isAuth = async (req, res, next) =>  {
+
+    try {
+
+        // get token from req
+        const token = req.cookies.token
+
+
+       // is token present 
+        if(!token) {
+
+            return res.status(401).json({
+                success : false ,
+                message : `please login `
+            })
+        }
+
+
+        // decoding token
+
+        const decode = jwt.verify(token , process.env.JWT_SECRET)
+
+        // after decode save token to req.user
+
+        req.user = decode
+
+        next()
+        
+        
+    } catch (error) {
+
+        return res.status(501).json({
+            success : false ,
+            message : `server Error for dev in file auth.middleware.js error = ${error}`
+        })
+        
+    }
+
+
+}
