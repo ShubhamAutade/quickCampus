8ca22be9725e74621e0e8118a25ec43ff159f43e
Shubham Autade <autades654@gmail.com>
2026-02-17 19:00:38 +0530

    feat(student): add oneCollege controller to fetch specific college details by ID

--- Backend/src/controllers/student.controllers.js
+++ Backend/src/controllers/student.controllers.js
@@ -1,5 +1,6 @@
 
 import {College} from "../models/College.model.js"
+import mongoose from "mongoose"
 
 
 // student Home page 
@@ -39,6 +40,69 @@ export const home = async (req , res) => {
         return res.status(500).json({
             success : false ,
             message : `server problem to fetch college to student home`
+        })
+        
+    }
+}
+
+
+// display one college appter click on college on home page 
+
+export const oneCollege =  async (req , res) => {
+    try {
+
+
+        // get college id from params
+        const {id} = req.params
+
+
+        // check id is present 
+
+        if(!id) {
+            return res.status(401).json({
+                success : false,
+                message : `please provide id`
+            })
+        }
+
+        // check provider id is rely soported to mongoDb
+
+        if(!mongoose.Types.ObjectId.isValid(id)) {
+            return res.status(401).json({
+                success : false,
+                message : `please provide valid id `
+            })
+        }
+
+        // id is valid then find college
+
+        const college = await College.findOne({_id : id}).select("name email contact profileImage city courses staff")
+        .lean()
+
+        // college is present 
+
+        if(!college) {
+            return res.status(400).json({
+                success : false,
+                message : `not found`
+            })
+        }
+
+
+         return res.status(200).json({
+                success : true,
+                message : `college found success fully`,
+                college
+            })
+
+
+
+        
+    } catch (error) {
+
+          return res.status(500).json({
+            success : false ,
+            message : `server problem to fetch particular college to student `
         })
         
     }
--- Backend/src/routes/student.routes.js
+++ Backend/src/routes/student.routes.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { isAuth } from "../middlewares/isAuth.middlewares.js"
 import { isRoleStudent } from "../middlewares/isRoleStudent.middlewares.js"
-import { home } from "../controllers/student.controllers.js"
+import { home, oneCollege } from "../controllers/student.controllers.js"
 
 const router = express.Router()
 
 router.get("/home", isAuth , isRoleStudent , home)
+
+router.get("/home/:id", isAuth , isRoleStudent, oneCollege)
 
 
 export default router
