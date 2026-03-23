d70042ce54dcc805a6672390d4a7fb4b544bcb73
Shubham Autade <autades654@gmail.com>
2026-03-23 18:58:48 +0530

    get use off Cookies.gat() to manage sate of flter

--- Frontend/src/storage/useFilterStore.js
+++ Frontend/src/storage/useFilterStore.js
@@ -1,25 +1,37 @@
-import zustand, { create } from "zustand"
+import { create } from "zustand";
 
-const useFilterStorage = create((set) => ({
+import Cookies from "js-cookie"
 
-    filters : {
-        city : "" , 
-        course  : "",
+// gat store cooks data  for save in filters 
+// because when we trying press apply button on filter the data will gon 
+// fro that we try this 
+
+const savedFilter = Cookies.get('studentFilter') ? JSON.parse(Cookies.get('studentFilter')) : null 
+
+export const useFilterStore = create((set) => ({
+    
+    isOpen: false,
+    toggleFilter: () => set((state) => ({ isOpen: !state.isOpen })),
+    closeFilter: () => set({ isOpen: false }),
+
+    
+    filters: savedFilter ||  {
+        requiredCity: "", 
+        requiredCourse: ""
+       
     },
 
-    setFilters : (data) => set ((state) => ({
-
-        filters : {...state.filters, ...data}
-
-    })),
     
+    setFilters: (data) => set((state) => ({
+        filters: { ...state.filters, ...data }
+    })),
 
-    resetFilter : () => set({
-        filters : {
-            city : "",
-            course : ""
+    
+    resetFilter: () => set({
+        filters: {
+            requiredCity: "",
+            requiredCourse: ""
+            
         }
     })
-
-
-}))
+}));
